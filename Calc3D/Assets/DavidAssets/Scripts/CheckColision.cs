211cf4d8ef665a76c0391c5a19c75f0b99c98508
Thavi4 <davidespondao@gmail.com>
2024-09-09 14:25:42 -0600

    Clase 1

---
+++ Calc3D/Assets/DavidAssets/Scripts/CheckColision.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckColision : MonoBehaviour
+{ 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Me choco la bala");
+    }
+
+    //private void ray
+}
